ccc75c0c8d1df445058cafb97949c83b655c1519
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-16 14:00:42 +0300

    Commenting previous binary search algorithm

--- array/search_algorithms/binary.py
+++ array/search_algorithms/binary.py
@@ -1,34 +1,37 @@
 
-def binary_search(arr, target):
+# def binary_search(arr, target):
     
-    left = 0
-    right = len(arr) - 1
+#     left = 0
+#     right = len(arr) - 1
     
-    if left <= right:
+#     if left <= right:
         
-        mid = (left + right) // 2
+#         mid = (left + right) // 2
         
-        if arr[mid] == target:
+#         if arr[mid] == target:
             
             
-            return mid
+#             return mid
         
-        elif arr[mid] < target:
-            left = mid + 1
+#         elif arr[mid] < target:
+#             left = mid + 1
         
-        else:
-            right = mid -1
-    return -1        
+#         else:
+#             right = mid -1
+#     return -1        
    
 
 
-array = [1, 2,3,5, 8, 13, 27]
-find = int(input("Search:"))
-print(f"{find}")
-result = binary_search(array, find) 
+# array = [1, 2,3,5, 8, 13, 27]
+# find = int(input("Search:"))
+# print(f"{find}")
+# result = binary_search(array, find) 
 
-if result != -1:
-    print("Value",find,"found at index", result)
-else:
-    print("Target not found in array.")           
-        
+# if result != -1:
+#     print("Value",find,"found at index", result)
+# else:
+#     print("Target not found in array.")           
+def locate_card(cards, query):
+    pass        
+
+  
