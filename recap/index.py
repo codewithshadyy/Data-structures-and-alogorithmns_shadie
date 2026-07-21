6a7465b2424bad3b90089e66e29dc4d817e80721
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-21 17:53:10 +0300

    implementing functions to basic inputs

--- recap/index.py
+++ recap/index.py
@@ -1,7 +1,17 @@
 
-name = input("What's your name?:")
-age = int(input("Whats your age:"))
+# name = str(input("What's your name?:"))
+# age = int(input("Whats your age:"))
 
-print(f"Hey {name} you are {age} years old")
+# print(f"Hey {name} you are {age} years old")
 
 
+def letWork():
+    name = str(input("What's your name?:"))
+    age = int(input("Whats your age:"))
+
+    print(f"Hey {name} you are {age} years old")
+    
+    
+    
+letWork()    
+    
