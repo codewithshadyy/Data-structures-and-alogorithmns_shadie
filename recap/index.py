5dac136c7adade7c3778acda52b7d79c9fca84a8
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-21 18:08:29 +0300

    random print

--- recap/index.py
+++ recap/index.py
@@ -5,13 +5,40 @@
 # print(f"Hey {name} you are {age} years old")
 
 
-def letWork():
-    name = str(input("What's your name?:"))
-    age = int(input("Whats your age:"))
+# def letWork():
+#     name = str(input("What's your name?:"))
+#     age = int(input("Whats your age:"))
 
-    print(f"Hey {name} you are {age} years old")
+#     print(f"Hey {name} you are {age} years old")
     
     
     
-letWork()    
+# letWork()  
+
+
+
+"""""
+
+PYthon Data types:
+
+we have python built in  Data Types
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+
+"""  
+
+import random
+
+def showRandom():
+    x = float(random.randrange(1, 10))
+    print(x)
+    
+showRandom()    
     
