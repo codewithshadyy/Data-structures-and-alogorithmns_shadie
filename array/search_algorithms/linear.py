06d2fefb8cf2b7de325f367bce54767f12e67892
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-08-05 20:49:41 +0300

    just the push

--- array/search_algorithms/linear.py
+++ array/search_algorithms/linear.py
@@ -1,20 +1,29 @@
 
-def search(arr, target):
+# def search(arr, target):
     
-    for i in range(len(arr)):
+#     for i in range(len(arr)):
         
-        if arr[i] == target:
-            print(f"Great your number {target} found")
-            return
-        else:
-            print("OOp! number not found dear")
-            return 
+#         if arr[i] == target:
+#             print(f"Great your number {target} found")
+#             return
+#         else:
+#             print("OOp! number not found dear")
+#             return 
     
-array = [7,89, 9, 11, 13,23]
-find = int(input("Enter a target number:"))
-print(f"{find}")
-searching = search(array, find)  
+# array = [7,89, 9, 11, 13,23]
+# find = int(input("Enter a target number:"))
+# print(f"{find}")
+# searching = search(array, find)  
 
-print(searching)  
+# print(searching)  
+
+
+names = ["moses", "kipson", "shadie"]
+
+nameIput = input("Serach name")
+
+for name in names:
+    if nameIput:
+        print(f"Name {names[name]} in {names}")
     
     
